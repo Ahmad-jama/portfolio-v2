3bc8df32fda43aa3cd5c978f2f81612dcb1cf35b
Ahmad-jama <m.jama221@gmail.com>
2022-05-22 17:13:36 +0300

    code done will starting the animation part soon

---
+++ README.md
@@ -0,0 +1,76 @@
+# Dopefolio 🔥
+
+## A Blazing Fast Multipage Portfolio Template for Developers 🚀
+
+## [Demo Link](https://ahmad-jama-v2.netlify.app) 🔗
+
+<div align="center">
+  <img src="assts/E-all" alt="home page" width="100%" />
+  <br>
+</div>
+
+<br/>
+<br/>
+
+## Features
+
+- ### **Easy to navigate 💯**
+- ### **beautiful design 🥳**
+- ### **No Additional Frameworks 🤘**
+- ### **No Additional Libraries 🙌**
+- ### **one Page 💎**
+- ### **Fully Responsive 🚀**
+- ### **Great Audits Score 🎖️**
+
+## <br/>
+
+<br/>
+
+## project used 🛠️
+
+- **HTML** 🚀
+- **CSS** 🚀
+- **JavaScript** 🚀
+- **Email.js** 🚀
+
+---
+
+<br/>
+
+## Support 😇
+
+If you find this portfolio good then please **support** me by donating some **Money** on the **Buy Me a Coffee** link below so I can continue chasing my dream of building useful **Open Source projects** that will help the developer community and the general audience and will allow me to change my life as well 😇
+
+### **[Buy Me A Coffee](https://www.buymeacoffee.com/rammcodes)** ❤️
+
+<br>
+
+---
+
+<br>
+
+## Give a Star ⭐
+
+If you like this project then give it a **Github** star by pressing the **Star** button ⭐
+
+<br>
+
+---
+
+<br>
+
+## Author 👨‍💻
+
+- **Ahmad jama** - **[Linkedin](https://www.linkedin.com/in/ahmad-jama)**, **[Watsapp](https://wa.me/+966572284393)**, **[Github](https://github.com/Ahmad-jama)**, **[Portfolio](https:/ahmad-jama-v2.netlify.app)**
+
+<br>
+
+#### Note:
+
+I'm currently looking for good **Job Opportunities** both **Remote ( Worldwide )** and **On-Site ( Anywhere in Saudi Arabia )**. So, if you have a good opportunity that matches my skills experience then you can contact me on my **[Linkedin](https://www.linkedin.com/in/ahmad-jama)** or my email id **m.jama221@gmail.com** 🙌
+
+<br>
+
+---
+
+<br>
---
+++ index.js
@@ -0,0 +1,191 @@
+const projectsContainer = document.querySelector(".projects__content");
+
+const projectsData = [
+  {
+    name: "E-Commerce",
+    description: [
+      "Built a full-stack web app to allow users to buy the products from the site",
+      "Used React to allow users to instantaneously update the cart interface without reloading.",
+      "build beautiful and mind-blowing user interfaces and high-performance component ",
+      "used Redux-Toolkit to store the states and save them in local storage",
+      "used firebase to fetch products and users can see product details",
+    ],
+    techStack: [
+      "Html",
+      "Css",
+      "Js",
+      "React",
+      "Redux",
+      "Redux-toolkit",
+      "Firebase",
+    ],
+    liveSite: "https://ahmad-ecommerce-project.netlify.app/",
+    github: "https://github.com/Ahmad-jama/E-commerce-app",
+    images: [
+      "assets/E-home.PNG",
+      "assets/E-show.PNG",
+      "assets/E-all.PNG",
+      "assets/E-cart.PNG",
+      "assets/E-single.PNG",
+    ],
+  },
+  {
+    name: "Blog",
+    description: [
+      "used Styled-components to build custom and reusable component",
+      "  use Firebase-storage to upload the images",
+      " upload the Article to firebase-firestore and display the content without reloading",
+      "users can write the article in markdown format and the site will convert it to jsx",
+    ],
+    techStack: [
+      "Html",
+      "Styled-components",
+      "Js",
+      "React",
+      "Firebase",
+      "React-icons",
+      "Firebase Storage",
+    ],
+
+    liveSite: "https://ahmad-jama-blog.netlify.app/",
+    github: "https://github.com/Ahmad-jama/blog-project",
+    images: [
+      "assets/B-home.PNG",
+      "assets/B-show.PNG",
+      "assets/B-all.PNG",
+      "assets/B-create.PNG",
+      "assets/B-single.PNG",
+    ],
+  },
+  {
+    name: "Netflex Clone",
+    description: [
+      " Retrieve and display movie from TMDB api ",
+      "Display in columns by genre ",
+      " The background of the banner changes when refresh",
+      "  When you click on a movie it will show detail about movie",
+    ],
+    techStack: [
+      "Html",
+      "Css",
+      "Ls",
+      "Movie-trailer",
+      "React",
+      "TMDB API",
+      "React Youtube",
+    ],
+
+    liveSite: "https://a-movie-site.netlify.app/",
+    github: "https://github.com/Ahmad-jama/full-netflix-clone",
+    images: [
+      "assets/N-home.PNG",
+      "assets/N-login.PNG",
+      "assets/N-start.PNG",
+      "assets/N-show.PNG",
+      "assets/N-single.PNG",
+    ],
+  },
+];
+
+projectsData.forEach((project) => {
+  projectsContainer.innerHTML += `
+ 
+ 
+ <div class="projects__row">
+ <div class="projects__row-img-cont">
+
+   <div class="carousel_wrapper">
+     <div class="carousel">
+       <div class="slide one">
+         <img src="${project.images[0]}"  loading="lazy"
+         />
+       </div>
+       <div class="slide two">
+         <img src="${project.images[1]}" loading="lazy"/>
+       </div>
+       <div class="slide three">
+         <img src="${project.images[2]}"    loading="lazy"/>
+       </div>
+       <div class="slide four">
+         <img src="${project.images[3]}"  loading="lazy"/>
+       </div>
+       <div class="slide five">
+         <img src="${project.images[4]}"    loading="lazy" />
+       </div>
+     </div>
+   </div>
+
+ </div>
+ <div class="projects__row-content">
+   <h3 class="projects__row-content-title">${project.name}</h3>
+   <ul class="projects__row-content-desc">
+${project.description.map((item) => "<li>" + item + "<li>").join(" ")}
+    </ul>
+    <h2 style='margin:2rem 0 1rem' > Stack used <h2>
+
+   <div class='stack'> ${project.techStack
+     .map((stack) => "<span>" + stack + "</span>")
+     .join(" ")} </div>
+
+
+<div class='buttons'>
+
+<a
+href=${project.liveSite}
+target="_blank"
+>Live site</a
+>
+
+<a
+href=${project.github}
+target="_blank"
+>Github</a
+>
+
+</div>
+
+
+ </div>
+</div>
+
+ 
+ `;
+});
+
+// start modal
+
+const modal = document.querySelector(".modal");
+const modalMessage = document.querySelector(".modal p");
+const modalClose = document.querySelector(".modal span");
+
+modalClose.addEventListener("click", () => {
+  modal.classList.remove("open");
+});
+
+// send email\
+
+window.onload = function () {
+  document
+    .querySelector(".my-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      emailjs.sendForm("default_service", "template_cidbtoi", this).then(
+        function () {
+          modal.classList.add("open");
+          modalMessage.textContent = "your meassage has send successfully ";
+        },
+        function (error) {
+          modal.classList.add("FAILED..." + error);
+        }
+      );
+    });
+};
+
+const testArray = ["one", "two", "tree"];
+
+console.log(testArray);
+
+const thenew = testArray.map((t) => t).join(" 3 ");
+
+console.log(thenew);
